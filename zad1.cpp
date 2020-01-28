fea17da50ec4eb7afdbb26d591e76cde15fee65f
Karol Guzikowski <karolguzikowski@gmail.com>
2020-01-28 21:58:26 +0100

    two modes are ready

--- zad1.cpp
+++ zad1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <shared_mutex>
+#include <mutex>
 #include <fstream>
 #include <string>
 #include <chrono>
@@ -12,12 +13,17 @@
 using namespace std;
 
 int getSeconds() {
-    return rand() % 5 + 1;;
+    return rand() % 1 + 1;
+}
+
+int getNaturalNumber() {
+    return rand() % 1000 + 1;
 }
 
 class Judge {
 public:
     shared_mutex decision;
+    mutex tie;
     Judge(){}
 };
 
@@ -27,13 +33,37 @@ public:
     Reader(string ID) {
         id = ID;
     }
-    void Action(fstream &file, shared_mutex &decision) {
+    void Action1(fstream &file, shared_mutex &decision) {
         string line;
-
+        int sec = getSeconds();
+        
+        this_thread::sleep_for(chrono::seconds(sec));
         shared_lock<shared_mutex> lock(decision);
-        this_thread::sleep_for(chrono::seconds(getSeconds()));
-        while (getline(file, line));
+
+        while (!file.eof()) {
+            file >> line;
+        }
+        file.clear();
+        file.seekg(0, ios::beg);
+
+        if(line.length() < 1) return;
+        cout << id + line << endl;
+
         lock.unlock();
+    }
+
+    void Action2(fstream &file, mutex &decision) {
+        string line;
+        int sec = getSeconds();
+        
+        this_thread::sleep_for(chrono::seconds(sec));
+        lock_guard<mutex> lock(decision);
+
+        while (!file.eof()) {
+            file >> line;
+        }
+        file.clear();
+        file.seekg(0, ios::beg);
 
         if(line.length() < 1) return;
         cout << id + line << endl;
@@ -46,14 +76,28 @@ public:
     Writer(string ID) {
         id = ID;
     }
-    void Action(fstream &file, shared_mutex &decision) {
-        int number = rand() % 1000 + 1;
+    void Action1(fstream &file, shared_mutex &decision) {
+        int sec = getSeconds();
 
+        this_thread::sleep_for(chrono::seconds(sec));
         lock_guard<shared_mutex> lock(decision);
-        this_thread::sleep_for(chrono::seconds(getSeconds()));
-        file << id + to_string(number) << endl;
+        file << id + to_string(getNaturalNumber()) << endl;
+    }
+
+    void Action2(fstream &file, mutex &decision) {
+        int sec = getSeconds();
+
+        this_thread::sleep_for(chrono::seconds(sec));
+        lock_guard<mutex> lock(decision);
+        file << id + to_string(getNaturalNumber()) << endl;
     }
 };
+
+
+/*
+    Action1 - zagłodzenie writerów
+    Action2 - bez zagłodzenie(remis)
+*/
 
 int main()
 {
@@ -66,6 +110,7 @@ int main()
     Judge judge = Judge();
     fstream file("dane.txt", ios::app | ios::in);
 
+    // zagłodzenie writerów
     if(file.is_open()) {
         for (int i = 0; i < num; i++)
         {
@@ -76,8 +121,10 @@ int main()
         }
         
         for (int i = 0; i < num; i++){
-            threads[i] = thread(&Writer::Action, &writers[i], ref(file), ref(judge.decision));
-            threads[i + num] = thread(&Reader::Action, &readers[i], ref(file), ref(judge.decision));
+            thread thWriter = thread(&Writer::Action1, &writers[i], ref(file), ref(judge.decision));
+            thread thReader = thread(&Reader::Action1, &readers[i], ref(file), ref(judge.decision));
+            threads.push_back(move(thWriter));
+            threads.push_back(move(thReader));
         }
 
         for(int i = 0; i < num * 2; i++) {
@@ -87,6 +134,31 @@ int main()
     } else {
         cout << "Couldn't open file: dane.txt!" << endl;
     }
+
+    // remis
+    // if(file.is_open()) {
+    //     for (int i = 0; i < num; i++)
+    //     {
+    //         string wID = to_string(0) + to_string(i);
+    //         string rID = to_string(1) + to_string(i);
+    //         writers.push_back(Writer(wID));
+    //         readers.push_back(Reader(rID));
+    //     }
+        
+    //     for (int i = 0; i < num; i++){
+    //         thread thWriter = thread(&Writer::Action2, &writers[i], ref(file), ref(judge.tie));
+    //         thread thReader = thread(&Reader::Action2, &readers[i], ref(file), ref(judge.tie));
+    //         threads.push_back(move(thWriter));
+    //         threads.push_back(move(thReader));
+    //     }
+
+    //     for(int i = 0; i < num * 2; i++) {
+    //         threads[i].join();
+    //     }
+    //     file.close();
+    // } else {
+    //     cout << "Couldn't open file: dane.txt!" << endl;
+    // }
 
     return 0;
 }
